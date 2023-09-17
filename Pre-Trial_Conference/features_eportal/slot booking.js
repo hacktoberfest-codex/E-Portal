14fc4ff64adbf2f0138ba689ec4e0baacc9e4607
sumitsamantara21 <121337160+sumitsamantara21@users.noreply.github.com>
2023-09-17 13:01:17 +0530

    slot booking

--- Pre-Trial_Conference/features_eportal/slot booking.js
+++ Pre-Trial_Conference/features_eportal/slot booking.js
@@ -1,41 +1,19 @@
-// Simulated data for available slots
-const availableSlots = [
-    { date: '2023-09-17', time: '10:00 AM' },
-    { date: '2023-09-17', time: '11:00 AM' },
-    { date: '2023-09-18', time: '9:00 AM' },
-    // Add more available slots here
-];
+document.addEventListener("DOMContentLoaded", function () {
+    const datePicker = document.getElementById("datePicker");
+    const timeSlot = document.getElementById("timeSlot");
+    const bookButton = document.getElementById("bookButton");
+    const bookingDate = document.getElementById("bookingDate");
+    const bookingTime = document.getElementById("bookingTime");
 
-// Function to generate the calendar
-function generateCalendar() {
-    const calendar = document.getElementById('calendar');
-    const currentDate = new Date();
-    const daysInMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
+    bookButton.addEventListener("click", function () {
+        const selectedDate = datePicker.value;
+        const selectedTime = timeSlot.value;
 
-    for (let day = 1; day <= daysInMonth; day++) {
-        const date = new Date(currentDate.getFullYear(), currentDate.getMonth(), day).toISOString().split('T')[0];
-        const dayElement = document.createElement('div');
-        dayElement.className = 'calendar-day';
-        dayElement.textContent = day;
-
-        if (isSlotAvailable(date)) {
-            dayElement.classList.add('available');
+        if (selectedDate && selectedTime) {
+            bookingDate.textContent = selectedDate;
+            bookingTime.textContent = selectedTime;
         } else {
-            dayElement.classList.add('unavailable');
-        }
-
-        calendar.appendChild(dayElement);
-    }
-}
-
-// Function to check if a slot is available
-function isSlotAvailable(date) {
-    for (const slot of availableSlots) {
-        if (slot.date === date) {
-            return true;
-        }
-    }
-    return false;
-}
-
-generateCalendar();
+            alert("Please select a date and time slot.");
+        }
+    });
+});
